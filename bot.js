870bfb8d51aadf82e96f00bdced3d6aa1e6f89de
SamehRFathy <46203153+SamehRFathy@users.noreply.github.com>
2019-01-07 08:54:44 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -421,7 +421,7 @@ client.on('message', message => {
      }
        });
 client.on('message', async message => {
-  if(message.content.startsWith(prefix + "اقتراح")) {
+  if(message.content.startsWith(prefix & "اقتراح")) {
   await  message.channel.send(`اكتب اقتراحك الان`)
     let filter = m => m.author.id === message.author.id
       var text = '';
@@ -435,5 +435,21 @@ client.on('message', async message => {
               })
             }
           }) 
+
+client.on('message', message => {
+   if(message.content.startsWith(prefix & "دعوات")) {
+    message.guild.fetchInvites().then(invs => {
+      let user = message.mentions.users.first() || message.author
+      let personalInvites = invs.filter(i => i.inviter.id === user.id);
+      let inviteCount = personalInvites.reduce((p, v) => v.uses + p, 0);
+               let mmmmEmbed = new Discord.RichEmbed()
+                         .setAuthor(client.user.username)
+                         .setThumbnail(message.author.avatarURL)
+ .addField(` لقد قمت بدعوة :`, ` ${inviteCount} `)
+           .setFooter(`- Requested By: ${message.author.tag}`);
+           message.channel.send(mmmmEmbed)
+});
+  }
+});
  
 client.login(process.env.BOT_TOKEN);
