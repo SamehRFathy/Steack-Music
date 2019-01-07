765ba1dc13cd4429d03ad9333e67447f202a57a2
SamehRFathy <46203153+SamehRFathy@users.noreply.github.com>
2019-01-07 07:26:45 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -272,6 +272,22 @@ ${prefix}queue ⇏ لمعرفة قآئمة التشغيل
    
    }
    });
+
+
+client.on('message', message => {
+   if (message.content === "~id") {
+   let embed = new Discord.RichEmbed()
+  .setColor("RANDOM")
+  .setThumbnail(message.author.avatarURL)
+  .addField("Name:",`${message.author.username}`, true)
+  .addField('Discrim:',"#" +  message.author.discriminator, true)
+  .addField("ID:", message.author.id, true)
+  .addField("Create At:", message.author.createdAt, true)
+     
+     
+  message.channel.sendEmbed(embed);
+    }
+});
  
  
  
