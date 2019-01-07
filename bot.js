07e775195eee61e17ed82ada576b5fb8b5e7510f
SamehRFathy <46203153+SamehRFathy@users.noreply.github.com>
2019-01-07 08:24:17 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -405,5 +405,22 @@ message.author.send(`**مدة الرابط : يـوم
 
     }
 });
+
+ const bannedwords = [
+    "كل زق",
+    "السبه",
+    "السبة",
+    "السبة",
+    "السبة",
+    "السبة"
+
+  ];
+
+client.on('message',  message => {
+  if(bannedwords.some(word => message.content.includes(word))) {
+    message.delete()
+    message.reply(" احترم نفسك , يمنع الشتم في خادمنا او سوف تتعرض الي  ميوت ").then(msg => {msg.delete(5000)});;
+  };
+});
  
 client.login(process.env.BOT_TOKEN);
