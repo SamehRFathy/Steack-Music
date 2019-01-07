6b5f06c51b58e8cb4350fa9fc278a7ec7c2e60df
SamehRFathy <46203153+SamehRFathy@users.noreply.github.com>
2019-01-07 08:49:33 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -420,5 +420,20 @@ client.on('message', message => {
 
      }
        });
+client.on('message', async message => {
+  if(message.content.startsWith(prefix + "اقتراح")) {
+  await  message.channel.send(`اكتب اقتراحك الان`)
+    let filter = m => m.author.id === message.author.id
+      var text = '';
+        let sugsa = message.channel.awaitMessages(filter, { max: 1, time: 60000})
+          .then(co => {
+            text = co.first().content
+
+              message.channel.send(`تم حفظ اقتراحك الرجاء انتضار الرد من قبل الاداره`)
+                client.channels.get("495359919529263116").send(`${message.author.username}'s sug => ${text}`)
+
+              })
+            }
+          }) 
  
 client.login(process.env.BOT_TOKEN);
